780979e3a489bb979e241d462511477d76287681
SviatSmol <37250837+SviatSmol@users.noreply.github.com>
2022-05-06 15:54:26 +0300

    Create ViewModel.swift

---
+++ ArabicNumberToRomanNumber/ViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  ViewModel.swift
+//  ArabicNumberToRomanNumber
+//
+//  Created by Sviat on 05.05.2022.
+//
+
+import Foundation
+
+
+class ConverterViewModel: ObservableObject {
+     @Published var arabic: String
+     @Published var roman: String
+     @Published var error: String
+ 
+     init() {
+         arabic = ""
+         roman = ""
+         error = ""
+    }
+
+    func convertNumber() {
+        if let n = Int(arabic) {
+            guard n > 0 && n < 10000 else {
+                roman = ""
+                error = "Number must be between 1 and 10000"
+                return
+            }
+            roman = Converter.convertArabicToRoman(n)
+        } else {
+                   roman = ""
+                   error = "'\(arabic)' is not a valid number"
+               }
+           }
+       }
