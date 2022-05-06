bbee49b852aef256b4ee5895cb5c4a43952bab9a
SviatSmol <37250837+SviatSmol@users.noreply.github.com>
2022-05-06 15:54:55 +0300

    Create ArabicNumberToRomanNumberApp.swift

---
+++ ArabicNumberToRomanNumber/ArabicNumberToRomanNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArabicNumberToRomanNumberApp.swift
+//  ArabicNumberToRomanNumber
+//
+//  Created by Sviat on 05.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ArabicNumberToRomanNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
